4ff1767d5618f65874904ded564d599df6cb4bf3
poememory <3269187297@qq.com>
2024-02-12 11:09:17 +0800

    feat:test-sign

--- src/app.config.ts
+++ src/app.config.ts
@@ -4,22 +4,22 @@ export default defineAppConfig({
   navigationBarTextStyle: 'black',
   navigationStyle: "custom",
   pages: [
-    'pages/index/index',
+    // 'pages/index/index',
 
-    'pages/teacher/login/login',
-    'pages/teacher/review/review',
-    'pages/teacher/personalInfo/personalInfo',
+    // 'pages/teacher/login/login',
+    // 'pages/teacher/review/review',
+    // 'pages/teacher/personalInfo/personalInfo',
 
-    'pages/student/prelogin/prelogin',
-    'pages/student/login/login',
-    'pages/student/application/application',
-    'pages/student/personalInfo/personalInfo',
+    // 'pages/student/prelogin/prelogin',
+    // 'pages/student/login/login',
+    // 'pages/student/application/application',
+    // 'pages/student/personalInfo/personalInfo',
 
-    'pages/sharing/adviceFeedback/adviceFeedback',
-    'pages/sharing/changingSteps/changinSteps',
-    'pages/student/application/details/approvalForm/approvalForm',
-    'pages/student/application/details/counsellorForm/counsellorForm',
-    'pages/student/application/details/departmentForm/departmentForm',
+    // 'pages/sharing/adviceFeedback/adviceFeedback',
+    // 'pages/sharing/changingSteps/changingSteps',
+    // 'pages/student/application/details/approvalForm/approvalForm',
+    // 'pages/student/application/details/counsellorForm/counsellorForm',
+    // 'pages/student/application/details/departmentForm/departmentForm',
     'pages/teacher/checking/checking'
   ],
 
