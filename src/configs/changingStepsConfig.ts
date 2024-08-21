1bdbf7a2e9facb11313e40b79eebdbf8ae2afdef
poememory <3269187297@qq.com>
2024-08-21 23:21:40 +0800

    fix:流程说明样式

--- src/configs/changingStepsConfig.ts
+++ src/configs/changingStepsConfig.ts
@@ -1,4 +1,4 @@
-export const changingImgs = [    'https://s2.loli.net/2024/08/15/SBIwCXavLRQdZVn.png',
+export const changingImgs = [   
     'https://s2.loli.net/2024/08/15/YBElpLvqr2geWTU.jpg',
     'https://s2.loli.net/2024/08/15/fGyFwxYkpojndSM.jpg',
     'https://s2.loli.net/2024/08/15/PCIOmFpHLgA9qo2.jpg'
